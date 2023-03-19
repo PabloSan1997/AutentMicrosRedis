a2edb3c0b5fc309eb41cccd5bd5199dd1ce2a56b
PabloSan1997 <jos_pab_997@hotmali.com>
2023-03-19 16:24:23 -0600

    Estructura principal

--- api/index.js
+++ api/index.js
@@ -1,1 +1,11 @@
 const express=require("express");
+const { api } = require("../config");
+const { user } = require("./components/network");
+
+const app = express();
+
+//Router
+app.use("/api/user", user);
+app.listen(api.port, ()=>{
+    console.log("http://localhost:"+api.port);
+});
