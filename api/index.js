2eff3b4abf4fe97060eed10137e1f412d44264a5
PabloSan1997 <jos_pab_997@hotmali.com>
2023-03-20 14:03:00 -0600

    Nuevis archivos

--- api/index.js
+++ api/index.js
@@ -1,11 +1,21 @@
-const express=require("express");
-const { api } = require("../config");
-const { user } = require("./components/network");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const swaggerUi = require('swagger-ui-express');
+
+const config = require('../config.js');
+const user = require('./components/user/network');
 
 const app = express();
 
-//Router
-app.use("/api/user", user);
-app.listen(api.port, ()=>{
-    console.log("http://localhost:"+api.port);
+app.use(bodyParser.json());
+
+const swaggerDoc = require('./swagger.json');
+
+// ROUER
+app.use('/api/user', user);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+app.listen(config.api.port, () => {
+    console.log('Api escuchando en el puerto ', config.api.port);
 });
